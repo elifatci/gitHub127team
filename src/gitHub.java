2eab129c60a812583cad2f90dbcf3fa9afaad478
Elif Atçı <eelif0600@gmail.com>
2023-09-28 12:43:11 +0300

    ikinciGun

--- src/gitHub.java
+++ src/gitHub.java
@@ -1,5 +1,6 @@
 public class gitHub {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("Push Kisayol");
     }
 }
---
+++ src/ikinciGun1.java
@@ -0,0 +1,5 @@
+public class ikinciGun1 {
+    public static void main(String[] args) {
+        System.out.println("Yeni bir islem yapiyorum");
+    }
+}
